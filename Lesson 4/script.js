abb1c45a5ded42fd90f2bd284b961827a5b8c48b
DEALV8808 <51203000+DEALV8808@users.noreply.github.com>
2019-06-09 17:39:32 +0400

    Add files via upload

---
+++ Lesson 4/script.js
@@ -0,0 +1,25 @@
+function main() {
+    var socket = io();
+    var chatDiv = document.getElementById('chat');
+    var input = document.getElementById('message');
+    var button = document.getElementById('submit');
+
+    function handleSubmit(evt) {
+        var val = input.value;
+        if (val != "") {
+            socket.emit("send message", val);
+        }
+    }
+    button.onclick = handleSubmit;
+
+    function handleMessage(msg) {
+        var p = document.createElement('p');
+        p.innerText = msg;
+        chatDiv.appendChild(p);
+        input.value = "";
+    }
+
+    socket.on('display message', handleMessage);
+}
+
+window.onload = main;   
